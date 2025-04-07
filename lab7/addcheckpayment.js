742ea309288025640892ebfac46fff9e062805d0
Brenden T. <144038546+bm17tala@users.noreply.github.com>
2025-04-06 20:04:19 -0400

    create lab 8 folder

--- lab7/addcheckpayment.js
+++ lab7/addcheckpayment.js
@@ -18,3 +18,39 @@ accountNumber.addEventListener('blur', () => {
 
     if (!regex.test(accountNumber.value)) alert("Please enter exactly 10 digits.");
 });
+
+const form = document.getElementById("incomeForm");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    submitForm();
+});
+
+async function submitForm(){
+
+
+    const formData = new FormData(document.getElementById("incomeForm"));
+    
+    try{
+        const res = await fetch("https://webhook-test.com/e9b7cec9a3e920d3b86d4bdd074855a8", {method: 'POST', body: formData});
+
+        const text = await res.text();
+        console.log(text);
+
+    }catch(error){
+        console.log(error);
+    }
+
+
+
+
+
+    // fetch("https://webhook-test.com/e9b7cec9a3e920d3b86d4bdd074855a8", {method: 'POST', body: formData})
+        // .then( (res) => res.text())
+        // .then( (response) => {
+        //     console.log(response);
+        // }).catch( (error) => {
+        //     console.log(error);
+        // });
+
+}
--- lab7/incomesource.js
+++ lab7/incomesource.js
@@ -10,4 +10,40 @@ if(amtValue <= 0){
     console.log(`$${amtValue}`);
 }
 
+const form = document.getElementById("incomeForm");
 
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    submitForm();
+});
+
+async function submitForm(){
+
+
+    const formData = new FormData(document.getElementById("incomeForm"));
+    
+    try{
+        const res = await fetch("https://webhook-test.com/e9b7cec9a3e920d3b86d4bdd074855a8", {method: 'POST', body: formData});
+
+        const text = await res.text();
+        console.log(text);
+
+    }catch(error){
+        console.log(error);
+    }
+
+    
+
+
+
+
+
+    // fetch("https://webhook-test.com/e9b7cec9a3e920d3b86d4bdd074855a8", {method: 'POST', body: formData})
+        // .then( (res) => res.text())
+        // .then( (response) => {
+        //     console.log(response);
+        // }).catch( (error) => {
+        //     console.log(error);
+        // });
+
+}
